a687443019b87904cf078031c3a58a8802ae848d
Shivang <83906239+shivang1809@users.noreply.github.com>
2022-11-08 13:55:31 +0530

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -3,10 +3,24 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry)=>{
         console.log(entry);
         if (entry.isIntersecting){
-            entry.target.classList.add('show');
+            entry.target.classList.add('show-left');
         }
     })
 })
 
-const hiddenElement = document.querySelectorAll('.hidden');
+const hiddenElement = document.querySelectorAll('.hidden-left');
 hiddenElement.forEach((el) => observer.observe(el))
+
+//-----------------------------------------------------------
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if (entry.isIntersecting){
+            entry.target.classList.add('show-up');
+        }
+    })
+})
+
+const hiddenElement2 = document.querySelectorAll('.hidden-up');
+hiddenElement2.forEach((el) => observer2.observe(el))
